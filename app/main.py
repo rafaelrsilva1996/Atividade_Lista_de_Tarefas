5b6635ec449ace5924ef7e1b6c0e29166b64eb66
Rafael Rodrigues <rafael.r.silva1996@gmail.com>
2022-05-14 23:19:40 -0300

    Segunda tela funcionando

--- app/main.py
+++ app/main.py
@@ -27,44 +27,67 @@ class MainWindow(Gtk.ApplicationWindow):
 
   def btn_press(self, widget):
     self.show_all()
-    
-    print(get_token_list_per_wallet(self.entry.props.text))
-    df = get_token_list_per_wallet(self.entry.props.text)
 
-    # Gtk.Application.do_startup(self)
-    win = SecondWindow(title="CryptoGames")
-    win.connect("destroy", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
-    win.do_process(df)
+    adress_wallet = self.entry.props.text
+    adress_wallet = '0x7d3d35878a8d6732a0abc296f0c57426c5a7d75e'
+    
+    print(get_token_list_per_wallet(adress_wallet))
+    df = get_token_list_per_wallet(adress_wallet)
+
+    self.do_process(dataframe=df, adress_wallet=adress_wallet)
+
+
+    # win = SecondWindow(title="CryptoGames")
+    # win.connect("destroy", Gtk.main_quit)
+    # print('Acionando do_process')
+    # win.do_process(dataframe=df, adress_wallet=adress_wallet)
+    # win.show_all()
 
     # SecondWindow(title="CryptoGames").do_process(dataframe=df)
 
 
-class SecondWindow(Gtk.ApplicationWindow):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+# class SecondWindow(Gtk.ApplicationWindow):
+#   def __init__(self, *args, **kwargs):
+#     super().__init__(*args, **kwargs)
 
-    self.set_default_size(500, 500)
+#     self.set_default_size(500, 500)
 
-    self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    self.add(self.box)
+  def do_process(self, dataframe, adress_wallet):
+    print('Entrou do_process')
+    print(self)
 
-  def do_process(self, dataframe):
+    win = Gtk.Window(title="CryptoGames")
+    win.set_default_size(800, 600)
+    win.connect("destroy", Gtk.main_quit)
     
-    self.lst_box = Gtk.ListBox()
-    self.box.pack_start(self.lst_box, False, True, 0)
 
-    for value in dataframe.values:
-      label_text = f'{value[0]}, {value[1]}, {value[2]}'
+    label = Gtk.Label(label = adress_wallet)
+    # self.box.pack_start(self.label, True, True, 0)
 
-      box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+    win.add(label)
 
-      label = Gtk.Label(label = label_text)
-      box.pack_start(label, True, True, 0)
+    win.show_all()
+    Gtk.main()
 
-      self.lst_box.insert(box, 0)
-      self.show_all()
+    # self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+    # self.label = Gtk.Label(label = adress_wallet)
+    # self.box.pack_start(self.label, True, True, 0)
+      
+    # self.lst_box = Gtk.ListBox()
+    # self.box.pack_start(self.lst_box, False, True, 0)
+
+    # for value in dataframe.values:
+    #   label_text = f'{value[0]}, {value[1]}, {value[2]}'      
+
+    #   self.label = Gtk.Label(label = label_text)
+    #   self.box.pack_start(self.label, True, True, 0)
+
+    #   self.lst_box.insert(self.box, 0)
+    #   self.show_all()
+    #   print(f'{value[0]}, {value[1]}, {value[2]}')
+    
+    print('FIM')
 
 
 class Application(Gtk.Application):
